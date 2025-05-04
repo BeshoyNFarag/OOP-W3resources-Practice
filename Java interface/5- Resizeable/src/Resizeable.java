60919aae657e0325dfe1e90dae733aa12fd748ab
Beshoy Farag <beshoynady401@gmail.com>
2025-05-04 13:13:00 +0200

    add 5th practice

---
+++ Java interface/5- Resizeable/src/Resizeable.java
@@ -0,0 +1,12 @@
+/*
+5. Write a Java program to create an interface Resizable with methods resizeWidth(int width)
+and resizeHeight(int height) that allow an object to be resized. Create a class Rectangle that
+implements the Resizable interface and implements the resize methods.
+*/
+
+
+
+public interface Resizeable {
+    void resizeWidth(int width);
+    void resizeHeight(int height);
+}
