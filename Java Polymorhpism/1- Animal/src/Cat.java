f8be85304ccdcc4ea7d6d254a14a86d034840c36
Beshoy Farag <beshoynady401@gmail.com>
2025-05-12 10:47:12 +0200

    add first pracitce

---
+++ Java Polymorhpism/1- Animal/src/Cat.java
@@ -0,0 +1,14 @@
+/*
+1. Write a Java program to create a base class Animal (Animal Family) with a method called Sound().
+Create two subclasses Bird and Cat. Override the Sound() method in each subclass to make a specific
+sound for each animal.
+*/
+
+public class Cat extends Animal {
+
+    @Override
+    public void sound(){
+
+        System.out.println(this.getClass().getName() + " Meow");
+    }
+}
