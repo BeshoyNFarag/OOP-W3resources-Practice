61a49a74a1484adce38527f30c60c013e6044c6a
Beshoy Farag <beshoynady401@gmail.com>
2025-05-13 17:59:03 +0200

    add 5th practice

---
+++ Java Polymorphism/5- Sports/src/Sports.java
@@ -0,0 +1,11 @@
+/*
+5. Write a Java program to create a base class Sports with a method called play().
+Create three subclasses: Football, Basketball, and Rugby. Override the play() method in each subclass
+to play a specific statement for each sport.
+*/
+
+
+public abstract class Sports {
+
+     public abstract void play();
+}
