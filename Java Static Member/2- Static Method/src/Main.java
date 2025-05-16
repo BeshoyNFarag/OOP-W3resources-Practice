6b5e3f3331ac8d1b361923e20682af905eabcb85
Beshoy Farag <beshoynady401@gmail.com>
2025-05-16 11:56:21 +0200

    add second practice

---
+++ Java Static Member/2- Static Method/src/Main.java
@@ -0,0 +1,19 @@
+/*
+2. Static Methods:
+Write a Java program to create a class called "MathUtility" with a static method add
+that takes two integers and returns their sum. Demonstrate the usage of this static
+method in the main method without creating an instance of "MathUtility".
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        MathUtility.add(2,4);
+        MathUtility.getSum();
+
+        MathUtility.add(5,6);
+        MathUtility.getSum();
+
+
+    }
+}
