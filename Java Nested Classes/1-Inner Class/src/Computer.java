3c8bef542b51ca0b5491cec4e2c1bf98eb05864f
Beshoy Farag <beshoynady401@gmail.com>
2025-05-18 20:31:04 +0200

    add 1st practice

---
+++ Java Nested Classes/1-Inner Class/src/Computer.java
@@ -0,0 +1,26 @@
+/*
+
+Write a Java program to create an outer class called Computer with an inner class Processor.
+The Processor class should have a method "displayDetails()" that prints the details of
+the processor (e.g., brand and speed). Create an instance of Processor from the Computer class
+and call the "displayDetails()" method.
+*/
+
+
+
+
+public class Computer {
+
+    class Processor {
+        private String brand = "Intel";
+        private int speed = 7;
+
+
+        public void displayDetails(){
+            System.out.println("Brand: " + brand + "\nSpeed: " + speed);
+        }
+
+
+    }
+
+}
