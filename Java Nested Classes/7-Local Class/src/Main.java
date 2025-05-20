54feff1977367a40fe258078e6e2d0e3ebea5824
Beshoy Farag <beshoynady401@gmail.com>
2025-05-20 18:32:13 +0200

    add 7th practice

---
+++ Java Nested Classes/7-Local Class/src/Main.java
@@ -0,0 +1,16 @@
+/*
+Write a Java program to create a class called House with a method calculateArea().
+Inside this method, define a local class Room with a method getArea() that calculates
+and returns the area of the room (length * width). Instantiate the Room class and call
+the getArea() method from within calculateArea().
+*/
+
+class Main{
+    public static void main(String[] args) {
+
+        House house = new House();
+        house.calculateArea();
+
+
+    }
+}
