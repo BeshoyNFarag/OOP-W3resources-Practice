085aae03674d76328592f3a68026c7238005849b
Beshoy Farag <beshoynady401@gmail.com>
2025-05-20 18:20:15 +0200

    add 6th practice

---
+++ Java Nested Classes/6-Static nested and method/src/MathUtil.java
@@ -0,0 +1,16 @@
+/*
+Write a Java program to create an outer class called 'MathUtil' with a static nested class Calculator.
+The Calculator class should have a static method add(int a, int b) that returns the sum of a and b.
+Call the add() method from the main method.
+*/
+
+
+public class MathUtil {
+
+
+    static class Calculator{
+        public static int add(int a, int b){
+            return a+b;
+        }
+    }
+}
